b89077ed25a5b2f41fc8ea2dfd18dbb449877e2e
Eliasz Łodyga <eliasz.firma@gmail.com>
2020-06-14 16:35:31 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const name = 'eliano';
+const age = 20;
+const city = 'Poznań';
+
+console.log(name)
+console.log(`Elo !Jestem ${name}. Mam ${age} lat. Mieszkam w stolicy Wielkoposki czyli ${city}iu`)

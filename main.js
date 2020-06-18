e4839992159e8009a64d509e4dc0716b55a1f0c8
Eliasz Łodyga <eliasz.firma@gmail.com>
2020-06-18 21:48:32 +0200

    huj wie, cośtam zmieniłem

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const name = 'eliano';
+const name = "eliano";
 const age = 20;
-const city = 'Poznań';
+const city = "Poznań";
 
-console.log(name)
-console.log(`Elo !Jestem ${name}. Mam ${age} lat. Mieszkam w stolicy Wielkoposki czyli ${city}iu`)
+console.log(name);
+console.log(
+  `Elo !Jestem ${name}. Mam ${age} lat. Mieszkam w stolicy Wielkoposki czyli ${city}iu`
+);

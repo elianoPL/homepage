71170b778388c874519fe31d9a0aaba07279ac35
Eliasz Łodyga <eliasz.firma@gmail.com>
2020-06-24 17:35:52 +0200

    footer content is generated by JS now

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
-const name = "eliano";
-const age = 20;
-const city = "Poznań";
-
-console.log(name);
-console.log(
-  `Elo !Jestem ${name}. Mam ${age} lat. Mieszkam w stolicy Wielkoposki czyli ${city}iu`
-);
+let footer = document.querySelector('.footer--js');
+footer.innerHTML = "&copy; Eliano 2020";

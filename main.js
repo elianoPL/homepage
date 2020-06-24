7ec74c3eaa166c4d3485bd0b6a6a2893ef756389
Eliasz Łodyga <eliasz.firma@gmail.com>
2020-06-24 20:30:45 +0200

    powitanie w konsoli

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 let footer = document.querySelector('.footer--js');
-footer.innerHTML = "&copy; Eliano 2020";
+let text = "&copy; Eliano 2020";
+footer.innerHTML = text;
+
+
+let imie = "ela";
+let wiek = 28;
+
+
+function uszanowanko(name, age) {
+
+  if(name.charAt(name.length-1)=="a") {
+    name = name.replace(/.$/,"u"); // Babskie imiona będą w odmienione w wołaczu !
+    
+  }
+  return `Witaj ${name}! Masz ${age} lat.`;
+}
+
+console.log(uszanowanko(imie, wiek));
